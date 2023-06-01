f5133c1fd7147af24f0deda420a98338fd4d6447
Sooraj Sannabhadti <2005sooraj@gmail.com>
2023-06-01 12:35:27 +0800

    Added 2nd Sensor

--- Feature Testing/ultrasonic_sensor_test.py
+++ Feature Testing/ultrasonic_sensor_test.py
@@ -1,6 +1,8 @@
 from gpiozero import DistanceSensor
 
+# https://gpiozero.readthedocs.io/en/stable/api_input.html
 # Don't put the GPIO pins to the SDA and SCL on the RPi, it should be on the normal GPIO pins
-sensor = DistanceSensor(echo=17, trigger=27)
+sensor1 = DistanceSensor(echo=17, trigger=27)
+sensor2 = DistanceSensor(echo=22, trigger=5)
 while True:
-    print(sensor.distance * 100, "cm")
+    print(sensor1.distance * 100, "cm &", sensor2.distance * 100, "cm")
