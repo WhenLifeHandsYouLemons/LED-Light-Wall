05fdaf12254091315f62376c8b467b7ea03a603b
Sooraj Sannabhadti <2005sooraj@gmail.com>
2023-06-08 14:08:38 +0800

    Small Changes

--- Feature Testing/ultrasonic_sensor_test.py
+++ Feature Testing/ultrasonic_sensor_test.py
@@ -1,3 +1,4 @@
+import math
 from gpiozero import DistanceSensor
 import matplotlib.pyplot as plt
 import time
@@ -8,9 +9,9 @@ sensor1 = DistanceSensor(echo=17, trigger=27)
 sensor2 = DistanceSensor(echo=22, trigger=23)
 sensor3 = DistanceSensor(echo=26, trigger=6)
 # All data is in cm
-# sensor1_data = []
-# sensor2_data = []
-# sensor3_data = []
+sensor1_data = []
+sensor2_data = []
+sensor3_data = []
 x_axis = []
 y_axis = []
 
@@ -21,31 +22,36 @@ end_time = 0
 DURATION = 10
 
 while end_time - start_time < DURATION:
+    # Get data from the sensors
     s1 = sensor1.distance
     s2 = sensor2.distance
     s3 = sensor3.distance
-    print(s1, s2)
+
+    # Save to a variable depending on which sensor is detecting the object
+    x = s1
+    if s2 > s3:
+        y = s3
+    else:
+        y = s2
+    # Weird bug but the sensor data only stores in the array if you print it out
+    print(x, y)
+
     # Calculate change in previous and current value
-#     s1_dydx = sensor1_data[-1] - s1
-#     s2_dydx = sensor2_data[-1] - s2
-#     s3_dydx = sensor3_data[-1] - s3
-#     if s1_dydx < 0:
-#         s1 *= -1
-#     if s2_dydx < 0:
-#         s2_dydx *= -1
+    if len(sensor1_data) > 0:
+        s1_dydx = math.fabs(sensor1_data[-1] - s1)
+        s2_dydx = math.fabs(sensor2_data[-1] - s2)
+        s3_dydx = math.fabs(sensor3_data[-1] - s3)
+    else:
+        s1_dydx, s2_dydx, s3_dydx = 0, 0, 0
+
     # Remove if outside allowed range
-#      and s1_dydx < 0.1 and s2_dydx < 0.1 and s3_dydx < 0.1
-    if s1 < 1 and (s2 < 1 or s3 < 1):
-#         sensor1_data.append(round(s1 * 100, 2))
-#         sensor2_data.append(round(s2 * 100, 2))
-#         sensor3_data.append(round(s3 * 100, 2))
-        x_axis.append(round(s1 * 100, 2))
-        if s1 >= s2:
-            y_axis.append(round(s2 * 100, 2))
-        else:
-            y_axis.append(round(s1 * 100, 2))
-            
-    # print(sensor1.distance * 100, "cm &", sensor2.distance * 100, "cm")
+    if s1 < 1 and (s2 < 1 or s3 < 1) and s1_dydx < 0.1 and s2_dydx < 0.1 and s3_dydx < 0.1:
+        sensor1_data.append(round(s1 * 100, 2))
+        sensor2_data.append(round(s2 * 100, 2))
+        sensor3_data.append(round(s3 * 100, 2))
+        x_axis.append(round(x * 100, 2))
+        y_axis.append(round(y * 100, 2))
+
     end_time = time.time()
 
 # Display data in matplotlib
