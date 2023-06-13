a663e38431750e94172e54610eb71d7cc1a9f556
Andy-L-1 <79822203+Andy-L-1@users.noreply.github.com>
2023-06-13 13:12:21 +0800

    Fixing code from ultrasonic_sensor_test and trimming errors/outliers of the data

--- Feature Testing/ultrasonic_sensor_test.py
+++ Feature Testing/ultrasonic_sensor_test.py
@@ -1,3 +1,4 @@
+import math
 from gpiozero import DistanceSensor
 import matplotlib.pyplot as plt
 import time
@@ -8,9 +9,6 @@ sensor1 = DistanceSensor(echo=17, trigger=27)
 sensor2 = DistanceSensor(echo=22, trigger=23)
 sensor3 = DistanceSensor(echo=26, trigger=6)
 # All data is in cm
-# sensor1_data = []
-# sensor2_data = []
-# sensor3_data = []
 x_axis = []
 y_axis = []
 
@@ -18,42 +16,73 @@ y_axis = []
 # Collect data for 10 seconds
 start_time = time.time()
 end_time = 0
-DURATION = 10
+DURATION = 3
 
+initial = 0
 while end_time - start_time < DURATION:
+    # Get data from the sensors
     s1 = sensor1.distance
     s2 = sensor2.distance
     s3 = sensor3.distance
-    print(s1, s2)
-    # Calculate change in previous and current value
-#     s1_dydx = sensor1_data[-1] - s1
-#     s2_dydx = sensor2_data[-1] - s2
-#     s3_dydx = sensor3_data[-1] - s3
-#     if s1_dydx < 0:
-#         s1 *= -1
-#     if s2_dydx < 0:
-#         s2_dydx *= -1
-    # Remove if outside allowed range
-#      and s1_dydx < 0.1 and s2_dydx < 0.1 and s3_dydx < 0.1
-    if s1 < 1 and (s2 < 1 or s3 < 1):
-#         sensor1_data.append(round(s1 * 100, 2))
-#         sensor2_data.append(round(s2 * 100, 2))
-#         sensor3_data.append(round(s3 * 100, 2))
-        x_axis.append(round(s1 * 100, 2))
-        if s1 >= s2:
-            y_axis.append(round(s2 * 100, 2))
+
+    # Check which sensor is detecting the object
+    x = s1
+    if s2 > s3:
+        if s2 - s3 < 10:
+            x = (s3 + (s2 - s3)/2)
         else:
-            y_axis.append(round(s1 * 100, 2))
-            
-    # print(sensor1.distance * 100, "cm &", sensor2.distance * 100, "cm")
+            y = s3
+    else:
+        if s3 - s2 < 10:
+            x = (s2 + (s3 - s2)/2)
+        else:
+            y = s2
+    # Weird bug but the sensor data only stores in the array if you print it out
+    # print(x, y)
+
+    # Calculate change in previous and current value
+    if len(y_axis) > 0:
+        dx = math.fabs(x - x_axis[-1])
+        dy = math.fabs(y - y_axis[-1])
+    else:
+        dx, dy = 0, 0
+        x = 0
+        y = 0
+
+    print(x, y, dx, dy)
+
+    # Remove if outside allowed range
+    add = True
+    #  and dx < 0.5 and dy < 0.5
+    if len(y_axis) > 0:
+        if x == x_axis[-1] and y == y_axis[-1]:
+            add = False
+
+    if initial < 10:
+        add = True
+        initial += 1
+
+    if s1 < 1 and (s2 < 1 or s3 < 1) and (add or (dx < 0.25 and dy < 0.25)):
+        x_axis.append(round(x, 4))
+        y_axis.append(round(y, 4))
+    else:
+    # elif (not add or not (dx < 0.25 and dy < 0.25)):
+        x_axis.append(x_axis[-1])
+        y_axis.append(y_axis[-1])
+
     end_time = time.time()
 
 # Display data in matplotlib
+i = 0
+while i < len(x_axis):
+    x_axis[i] *= 100
+    y_axis[i] *= 100
+    i += 1
 print(x_axis, y_axis)
 
 DEBUG = True
 if DEBUG:
-    plt.plot(x_axis, y_axis)
+    plt.plot(x_axis, y_axis, "ro")
     plt.title("Field of View of the Ultrasonic Sensors")
     plt.grid(True)
     plt.xlim([0, 100])
