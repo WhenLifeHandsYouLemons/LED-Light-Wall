8cf8baf64c2833a5b83d29eecdada2db084734ea
Sooraj Sannabhadti <2005sooraj@gmail.com>
2023-06-27 15:39:26 +0800

    Moved temp display code to new file

--- rpi_code.py
+++ rpi_code.py
@@ -1202,45 +1202,9 @@ Main loop
 # Reset board
 setAllPixelsColour(pixels, COLOURS["Black"])
 
-# Compute test waves
-# merge1 = []
-# merge1.append(precomputeColours(precomputeRain(10), COLOURS["Dark Blue"], COLOURS["Black"], 7))
-# merge1.append(precomputeColours(precomputeRain(11), COLOURS["Dark Blue"], COLOURS["Black"], 7))
-# merge1.append(precomputeColours(precomputeRain(16), COLOURS["Dark Blue"], COLOURS["Black"], 7))
-# merge1.append(precomputeColours(precomputeRain(5), COLOURS["Dark Blue"], COLOURS["Black"], 7))
-# merge1.append(precomputeColours(precomputeRain(23), COLOURS["Dark Blue"], COLOURS["Black"], 7))
-# merge1.append(precomputeColours(precomputeRain(1), COLOURS["Dark Blue"], COLOURS["Black"], 7))
-# merge1.append(precomputeColours(precomputeRain(29), COLOURS["Dark Blue"], COLOURS["Black"], 7))
-# merged1 = mergeWaves(merge1, [0, 3, 4, 10, 2, 7, 0])
-# 
-# merge2 = []
-# merge2.append(precomputeColours(precomputeCircularWave(15, 10, 9), COLOURS["Red"], COLOURS["Black"], 3))
-# merge2.append(precomputeColours(precomputeWave(1, 29), COLOURS["Red Orange"], COLOURS["Black"], 6))
-# merge2.append(precomputeColours(precomputeRipple(10, 6, 10), COLOURS["Lime"], COLOURS["Black"], 4))
-# merged2 = mergeWaves(merge2, [5, 0, 7])
-
 # Main running loop
 x, y = -1, -1
 while True:
-#     setAllPixelsColour(pixels, COLOURS["Black"])
-#     pixels, pixel_framebuf = changeBrightness(1)
-#     displayWave(merged1, 0.05)
-# 
-#     setAllPixelsColour(pixels, COLOURS["Black"])
-# #     pixels, pixel_framebuf = changeBrightness(0.3)
-#     displayWave(merged2, 0.08)
-# 
-#     setAllPixelsColour(pixels, COLOURS["Black"])
-#     drawText("CLASS", COLOURS["Dark Blue"], 0, 2)
-#     drawText("2023", COLOURS["Purple"], 4, 11)
-#     time.sleep(5)
-# 
-#     setAllPixelsColour(pixels, COLOURS["Black"])
-#     drawText("LED", COLOURS["Lime"], 7, 2)
-#     drawText("WALL", COLOURS["Yellow"], 4, 11)
-#     time.sleep(5)
-
-
     # To use ultrasonic sensors
     # Store the previous x and y values
     p_x, p_y = x, y
